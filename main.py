d438af42ba9b366bde0e94ed680e85137d7249bb
nyanyacyan <nyanyacyan@gmail.com>
2023-09-05 09:15:04 +0900

    Noneの可能性がある部分を修正

--- main.py
+++ main.py
@@ -754,6 +754,9 @@ def generate_html_content(file_path):
     specific_word = word_entry.get()
     df = pd.read_csv(file_path)
 
+    # Noneを空文字列で埋める
+    df.fillna('', inplace=True)
+
     url = 'https://iminain.com/%e3%80%90%e5%a4%89%e6%95%b0ver%e3%80%91%e7%b4%a0%e6%b0%b4%e3%81%af%e6%84%8f%e5%91%b3%e3%81%aa%e3%81%84%ef%bc%9f%e6%84%8f%e5%91%b3%e3%81%82%e3%82%8b%ef%bc%9f%e8%a9%95%e5%88%a4%e3%81%a8%e3%81%8a/'
 
     # fetch_html_templateでhtml_templateを初期化 
