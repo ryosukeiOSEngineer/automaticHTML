3c47336662a6a80f78e49850bad9837c6997a031
nyanyacyan <nyanyacyan@gmail.com>
2023-09-18 07:30:52 +0900

    1,2,4,7,8を追加実施。デバック済み

--- updateMain.py
+++ updateMain.py
@@ -62,15 +62,15 @@ def deleteSection3_2(html_template, start_marker="<!-- 3-2_DELETE_START -->", en
 
 
 # 5-1 削除
-def deleteSection5_1(html_template, start_marker="<!-- 5-1_DELETE_START -->", end_marker="<!-- 5-1_DELETE_END -->"):
-    deleted_part5_1 = re.escape(start_marker) + "(.*?)" + re.escape(end_marker)
-    return re.sub(deleted_part5_1, '', html_template, flags=re.DOTALL)
+# def deleteSection5_1(html_template, start_marker="<!-- 5-1_DELETE_START -->", end_marker="<!-- 5-1_DELETE_END -->"):
+#     deleted_part5_1 = re.escape(start_marker) + "(.*?)" + re.escape(end_marker)
+#     return re.sub(deleted_part5_1, '', html_template, flags=re.DOTALL)
 
 
-# 6-1 削除
-def deleteSection6_1(html_template, start_marker="<!-- 6-1_DELETE_START -->", end_marker="<!-- 6-1_DELETE_END -->"):
-    deleted_part6_1 = re.escape(start_marker) + "(.*?)" + re.escape(end_marker)
-    return re.sub(deleted_part6_1, '', html_template, flags=re.DOTALL)
+# 6-1 削除    調整必須
+# def deleteSection6_1(html_template, start_marker="<!-- 6-1_DELETE_START -->", end_marker="<!-- 6-1_DELETE_END -->"):
+#     deleted_part6_1 = re.escape(start_marker) + "(.*?)" + re.escape(end_marker)
+#     return re.sub(deleted_part6_1, '', html_template, flags=re.DOTALL)
 
 
 # ------------検索部分の定義----------------
@@ -113,8 +113,279 @@ def replace_1_red(html_template, df):
 
 
 # <!-- 1-BLUE -->
+# 置換を定義する関数
+def define_1_blue(df): 
+    try: # もし成功したら
+        csv_data_1_blue = df.loc[1, '8. 水素水のメリットを3つ教えてください']
+        csv_split_list = re.split('、|。|\n', csv_data_1_blue)
+        csv_list_customize = ['「' + item + '」' for item in csv_split_list if item]
+        csv_split_join = ' '.join(csv_list_customize)
+        html_insert_1_blue = f'<p>意味があるとされるおすすめポイントは{csv_split_join}などがあります。</p>'
+        return html_insert_1_blue
+    except Exception as e: # もし失敗したら
+        print(f"1-REDの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_1_blue(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_1_blue(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 1-BLUE -->\s*<p>).*?(</p>)'
+
+    updated_html_part_1_blue = re.sub(pattern, f'\\1{html_insert[3:]}\\2', html_template)
+    print("1-BLUEの置換が成功しました。")
+    return updated_html_part_1_blue
+
 
 # <!-- 1-GREEN -->
+# 置換を定義する関数
+def define_1_green(df): 
+    try: # もし成功したら
+        csv_data_1_green = df.loc[2:, '8. 水素水のメリットを3つ教えてください'].tolist()
+        csv_split_list = []
+        for item in csv_data_1_green:
+            csv_split_list.extend(re.split('、|。|\n', str(item)))
+        csv_list_customize = ['<li>' + item + '</li>' for item in csv_split_list if item]
+        csv_split_join = '\n\n\n\n'.join(csv_list_customize)
+        
+        html_insert_1_green = f'<ul class="is-style-check_list has-swl-pale-02-background-color has-background">\n{csv_split_join}\n</ul>'
+        return html_insert_1_green
+    except Exception as e: # もし失敗したら
+        print(f"1-GREENの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_1_green(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_1_green(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 1-GREEN -->\s*<ul class="is-style-check_list has-swl-pale-02-background-color has-background">).*?(</ul>)'
+
+    updated_html_part_1_green = re.sub(pattern, html_insert, html_template, flags=re.DOTALL)
+
+    print("1-GREENの置換が成功しました。")
+    return updated_html_part_1_green
+
+
+# <!-- 2-RED -->
+# 置換を定義する関数
+def define_2_red(df): 
+    try: # もし成功したら
+        csv_data_2_red = df.loc[0, '13. どんな人におすすめですか？3つ教えてください']
+        csv_split_list = re.split('、|。|\n', csv_data_2_red)
+        csv_list_customize = ['<li>' + item + '</li>' for item in csv_split_list if item]
+        csv_split_join = '\n\n\n\n'.join(csv_list_customize)
+        
+        html_insert_2_red = f'<ul class="is-style-check_list">\n{csv_split_join}\n</ul>'
+        return html_insert_2_red
+    except Exception as e: # もし失敗したら
+        print(f"2-REDの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_2_red(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_2_red(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 2-RED -->\s*<ul class="is-style-check_list">).*?(</ul>)'
+
+    updated_html_part_2_red = re.sub(pattern, html_insert, html_template, flags=re.DOTALL)
+
+    print("2-REDの置換が成功しました。")
+    return updated_html_part_2_red
+
+
+# <!-- 4-RED -->
+# 置換を定義する関数
+def define_4_red(df): 
+    try: # もし成功したら
+        csv_data_4_red = df.loc[0, '11. 水素水のデメリットを3つ教えてください']
+        csv_split_list = re.split('、|。|\n', csv_data_4_red)
+        csv_list_customize = ['「' + item + '」' for item in csv_split_list if item]
+        csv_split_join = ' '.join(csv_list_customize)
+        html_insert_4_red = f'<p>意味ない理由として{csv_split_join}などのコメントがありました。</p>'
+        return html_insert_4_red
+    except Exception as e: # もし失敗したら
+        print(f"4-REDの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_4_red(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_4_red(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 4-RED -->\s*<p>).*?(</p>)'
+
+    updated_html_part_4_red = re.sub(pattern, f'\\1{html_insert[3:]}\\2', html_template)
+    print("4-REDの置換が成功しました。")
+    return updated_html_part_4_red
+
+
+# <!-- 4-BLUE -->
+# 置換を定義する関数
+def define_4_blue(df): 
+    try: # もし成功したら
+        csv_data_4_blue = df.loc[1:, '11. 水素水のデメリットを3つ教えてください'].tolist()
+        csv_split_list = []
+        for item in csv_data_4_blue:
+            csv_split_list.extend(re.split('、|。|\n', str(item)))
+        csv_list_customize = ['<li>' + item + '</li>' for item in csv_split_list if item]
+        csv_split_join = '\n\n\n\n'.join(csv_list_customize)
+        
+        html_insert_4_blue = f'<ul class="is-style-triangle_list">\n{csv_split_join}\n</ul>'
+        return html_insert_4_blue
+    except Exception as e: # もし失敗したら
+        print(f"4-BLUEの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_4_blue(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_4_blue(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 4-BLUE -->\s*<ul class="is-style-triangle_list">).*?(</ul>)'
+
+    updated_html_part_4_blue = re.sub(pattern, html_insert, html_template, flags=re.DOTALL)
+
+    print("4-BLUEの置換が成功しました。")
+    return updated_html_part_4_blue
+
+
+# <!-- 7-RED -->
+# 置換を定義する関数
+def define_7_red(df): 
+    try: # もし成功したら
+        csv_data_7_red = df.loc[0, '13. どんな人におすすめですか？3つ教えてください']
+        csv_split_list = re.split('、|。|\n', csv_data_7_red)
+        csv_list_customize = ['「' + item + '」' for item in csv_split_list if item]
+        csv_split_join = ' '.join(csv_list_customize)
+        html_insert_7_red = f'<p>特に{csv_split_join}におすすめです。</p>'
+        return html_insert_7_red
+    except Exception as e: # もし失敗したら
+        print(f"7-REDの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_7_red(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_7_red(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 7-RED -->\s*<p>).*?(</p>)'
+
+    updated_html_part_7_red = re.sub(pattern, f'\\1{html_insert[3:]}\\2', html_template)
+    print("7-REDの置換が成功しました。")
+    return updated_html_part_7_red
+
+# <!-- 7-BLUE -->
+# 置換を定義する関数
+def define_7_blue(df): 
+    try: # もし成功したら
+        csv_data_7_blue = df.loc[1:, '13. どんな人におすすめですか？3つ教えてください'].tolist()
+        csv_split_list = []
+        for item in csv_data_7_blue:
+            csv_split_list.extend(re.split('、|。|\n', str(item)))
+        csv_list_customize = ['<li>' + item + '</li>' for item in csv_split_list if item]
+        csv_split_join = '\n\n\n\n'.join(csv_list_customize)
+        
+        html_insert_7_blue = f'<ul class="is-style-check_list">\n{csv_split_join}\n</ul>'
+        return html_insert_7_blue
+    except Exception as e: # もし失敗したら
+        print(f"7-BLUEの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_7_blue(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_7_blue(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 7-BLUE -->\s*<ul class="is-style-check_list">).*?(</ul>)'
+
+    updated_html_part_7_blue = re.sub(pattern, html_insert, html_template, flags=re.DOTALL)
+
+    print("7-BLUEの置換が成功しました。")
+    return updated_html_part_7_blue
+
+
+# <!-- 8-RED -->
+# 置換を定義する関数
+def define_8_red(df): 
+    try: # もし成功したら
+        csv_data_8_red = df.loc[0, '8. 水素水のメリットを3つ教えてください']
+        csv_split_list = re.split('、|。|\n', csv_data_8_red)
+        csv_split_join = '、'.join(csv_split_list)
+        html_insert_8_red = f'<p>XXXは{csv_split_join}のが魅力です。【意味ナイン】では、XXXは意味ないのか、意味あるのか調査し、評判やコメント、おすすめ代替案について紹介しています。</p>'
+        return html_insert_8_red
+    except Exception as e: # もし失敗したら
+        print(f"8-REDの置換生成に失敗しました: {e}")
+        return None
+
+# ファイルを読み込んで置換を実施する関数定義
+def replace_8_red(html_template, df): 
+    if df is None: # ファイルデータが読み込まれたか確認
+        print("データフレームのロードに失敗しました。")
+        return html_template
+    
+    html_insert = define_8_red(df)
+    if html_insert is None:
+        print("置換に失敗しました。")
+        return html_template
+    
+    # コメントタグの直後の p タグを置換する正規表現パターン
+    pattern = r'(<!-- 8-RED -->\s*<p>).*?(</p>)'
+
+    updated_html_part_8_red = re.sub(pattern, f'\\1{html_insert[3:]}\\2', html_template)
+    print("8-REDの置換が成功しました。")
+    return updated_html_part_8_red
 
 # ------------検索部分の置換するための関数定義----------------
 
@@ -123,14 +394,20 @@ def replace_1_red(html_template, df):
 def delete_sections(html_template):
     html_template = deleteSection3_1(html_template)
     html_template = deleteSection3_2(html_template)
-    html_template = deleteSection5_1(html_template)
-    html_template = deleteSection6_1(html_template)
+    # html_template = deleteSection5_1(html_template)
+    # html_template = deleteSection6_1(html_template)
     return html_template
 
 def replace_sections(html_template, df):
     html_template = replace_1_red(html_template, df)
-
-    
+    html_template = replace_1_blue(html_template, df)
+    html_template = replace_1_green(html_template, df)
+    html_template = replace_2_red(html_template, df)
+    html_template = replace_4_red(html_template, df)
+    html_template = replace_4_blue(html_template, df)
+    html_template = replace_7_red(html_template, df)
+    html_template = replace_7_blue(html_template, df)
+    html_template = replace_8_red(html_template, df)
     # 他の置換処理もここで行う
     return html_template
 
